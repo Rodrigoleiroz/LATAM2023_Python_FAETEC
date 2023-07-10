cb60f746953208c1e72533d386b0fc3b377e890a
Rodrigo Leiroz <rodrigoleiroz.devops@gmail.com>
2023-07-10 12:07:31 -0300

    comparacao e condicional

--- if_na_pratica_1.py
+++ if_na_pratica_1.py
@@ -13,4 +13,4 @@ if maior_numero < valor2:
 elif maior_numero < valor3:
     maior_numero = valor3
 
-print ("O maior numero é igual a ", maior_numero)
+print ("O maior numero é igual a", maior_numero)
