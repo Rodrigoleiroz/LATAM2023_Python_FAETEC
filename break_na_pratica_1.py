c177db7915b739418b135bb4275edebaa01240d7
Rodrigo Leiroz <rodrigoleiroz.devops@gmail.com>
2023-07-13 12:06:51 -0300

    break 1th exercicio

---
+++ break_na_pratica_1.py
@@ -0,0 +1,17 @@
+# A declaração break é usada para sair/encerrar um loop.
+
+# Projete um programa que use um loop while e solicite continuamente que o usuário insira uma palavra,
+# a menos que o usuário insira "chupacabra" como a palavra de saída secreta, caso em que a mensagem "Você saiu do loop com sucesso". 
+# Deve ser impresso na tela, e o loop deve terminar. Não imprima nenhuma das palavras inseridas pelo usuário. 
+# Use o conceito de execução condicional e a declaração break.
+
+while True:
+    palavra_inserida = input("Digite uma palavra: ")
+
+    if palavra_inserida == "chupacabra":
+        break
+    
+print ("Você saiu do loop com sucesso")
+
+
+
