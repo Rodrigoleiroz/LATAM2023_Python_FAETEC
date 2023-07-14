f8aeb3ebd955b317199deb23870182cceb04cc49
Rodrigo Leiroz <rodrigoleiroz.devops@gmail.com>
2023-07-13 22:48:20 -0300

    enunciado loopes 3th exercicio

--- loops_na_pratica_3.py
+++ loops_na_pratica_3.py
@@ -1,3 +1,15 @@
+# Ouça esta história: um garoto e seu pai, um programador de computador, estão jogando com blocos de madeira. Eles estão construindo uma pirâmide.
+
+# A pirâmide deles é um pouco esquisita, pois na verdade é uma parede em forma de pirâmide - é plana. A pirâmide é empilhada de acordo com um princípio simples: 
+
+# cada camada inferior contém um bloco a mais do que a camada acima.
+
+# Sua tarefa é escrever um programa que lê o número de blocos que os construtores têm e gera a altura da pirâmide que pode ser construída usando esses blocos.
+
+# Nota: a altura é medida pelo número de camadas totalmente concluídas; se os construtores não tiverem um número suficiente de blocos e não puderem concluir a 
+
+# próxima camada, eles terminarão seu trabalho imediatamente.
+
 blocos = int(input("Digite o número de blocos: "))
 
 altura = 0
